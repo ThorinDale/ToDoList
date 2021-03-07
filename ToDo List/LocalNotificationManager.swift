f318076303f1512e75180fa783a2853c07093b7e
Darren Zheng <zhengdarren9@gmail.com>
2021-03-07 13:09:37 -0500

    finishing touches

--- ToDo List/LocalNotificationManager.swift
+++ ToDo List/LocalNotificationManager.swift
@@ -5,12 +5,12 @@
 //  Created by Darren Zheng on 3/6/21.
 //
 
-import Foundation
+import UIKit
 import UserNotifications
 
 struct LocalNotificationManager {
     
-    static func authorizeLocalNotifications() {
+    static func authorizeLocalNotifications(viewController: UIViewController) {
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
             guard error == nil else {
                 print("ERROR: \(error!.localizedDescription)")
@@ -20,7 +20,26 @@ struct LocalNotificationManager {
                 print("Notification Authorization Granted!")
             } else {
                 print("The user has denied notifications!")
-                // TODO: Put an alert in here telling the user what to do
+                DispatchQueue.main.async {
+                    viewController.oneButtonAlert(title: "User Has Not Allowed Notifications", message: "To receive alerts for reminders, open the Settings app, select To Do List > Notifications > Allow Notifications")
+                }
+            }
+        }
+    }
+    
+    static func isAuthorized(completed: @escaping (Bool)->()) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            guard error == nil else {
+                print("ERROR: \(error!.localizedDescription)")
+                completed(false)
+                return
+            }
+            if granted {
+                print("Notification Authorization Granted!")
+                completed(true)
+            } else {
+                print("The user has denied notifications!")
+                completed(false)
             }
         }
     }
